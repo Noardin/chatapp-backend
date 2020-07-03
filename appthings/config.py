a2d865a8d8b86d8dcb314bcad38541e6508650bc
Noardin <triksiand@gmail.com>
2020-07-03 15:56:15 +0200

    veryfied sender

--- appthings/config.py
+++ appthings/config.py
@@ -4,12 +4,13 @@ from os.path import join, dirname, realpath
 class BaseConfig(object):
     SECRET_KEY = 'my_secret_key'
     SQLALCHEMY_DATABASE_URI='sqlite:///chat.db'
-    MAIL_DEFAULT_SENDER = 'triksiand@gmail.com'
+    MAIL_DEFAULT_SENDER = 'chatapp.assistant@gmail.com'
     # email server
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
+    MAIL_USERNAME = 'chatapp.assistant'
     CORS_HEADERS = 'Content-Type'
     UPLOAD_FOLDER = join(dirname(realpath(__file__)), '../static/img/profile_img')
     UPLOAD_FOLDER_AUDIO = join(dirname(realpath(__file__)), '../static/audio_messages')
